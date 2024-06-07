08a60b57c27566e185ce0a0eb3586a4d303709b7
Cris Mansilla <mansillalizamacristian>
2024-06-06 22:39:20 -0300

    tercera

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+  res.render('index', {
+    novedades
+  });
 });
 
 router.post('/', async (req, res, next) => {
@@ -20,7 +25,7 @@ router.post('/', async (req, res, next) => {
   var obj = {
     to: 'mansillalizamacristian@gmail.com',
     subject: 'Mensaje desde la web de Pelitos Contentos',
-    html: nombre + " " + apellido + " se ha comunicado a través de la web con el siguiente mensaje: " + mensaje +", su correo es: " + email + ", su teléfono es: " + tel + ", y nos escribe desde la ciudad de " + ciudad + " provincia de " + provincia + "."
+    html: nombre + " " + apellido + " se ha comunicado a través de la web con el siguiente mensaje: " + mensaje + ", su correo es: " + email + ", su teléfono es: " + tel + ", y nos escribe desde la ciudad de " + ciudad + " provincia de " + provincia + "."
   }
 
   var transport = nodemailer.createTransport({
